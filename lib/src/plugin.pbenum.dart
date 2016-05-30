2ca57be1ddee167e7d0c9afd7e0694bd80986a8c
Kevin Moore <kevmoo@google.com>
2016-05-30 18:27:28 +0200

    update generated files

---
+++ lib/src/plugin.pbenum.dart
@@ -0,0 +1,5 @@
+///
+//  Generated code. Do not modify.
+///
+library proto2.compiler_plugin_pbenum;
+
---
+++ lib/src/plugin.pbserver.dart
@@ -0,0 +1,7 @@
+///
+//  Generated code. Do not modify.
+///
+library proto2.compiler_plugin_pbserver;
+
+export 'plugin.pb.dart';
+
